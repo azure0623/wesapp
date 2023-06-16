454f8a820375eee1745cb13026cd7eb90baf51f3
Vikrant Kumar <azure.devops0623@gmail.com>
2023-06-16 14:19:45 +0530

    initial commit.

---
+++ webapplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace webapplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
